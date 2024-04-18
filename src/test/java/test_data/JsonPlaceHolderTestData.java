4ec943a72674f277c7348c429c4fec84e8c53a8d
Melad A <melad.alamri@gmail.com>
2024-04-18 15:16:19 +0300

    Test data for JsonPlace

---
+++ src/test/java/test_data/JsonPlaceHolderTestData.java
@@ -0,0 +1,15 @@
+package test_data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class JsonPlaceHolderTestData {
+
+    public static Map<String, Object> expectedDataMap(Integer userId, String title, Boolean completed){
+        Map<String, Object> expectedData = new HashMap<>();
+        expectedData.put( "userId", userId);
+        expectedData.put( "title", title);
+        expectedData.put( "completed", completed);
+        return expectedData;
+    }
+}
